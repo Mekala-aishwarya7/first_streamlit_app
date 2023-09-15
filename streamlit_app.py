bbd4ff8056e57f796c07e387dfe36c5daeb2c92f
Mekala-aishwarya7 <145124330+Mekala-aishwarya7@users.noreply.github.com>
2023-09-15 17:42:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,6 @@ streamlit.title("my parents new healthy dinner")
 
 streamlit.header('breakfast Menu')
 
-
 streamlit.body('dosa & chutney')
-
 streamlit.body('puri & chutney')
-
 streamlit.body('bread')
