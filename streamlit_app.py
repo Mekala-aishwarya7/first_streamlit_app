922f003b1cde9a81bf4eadbbf6de34add4fc2c9d
Mekala-aishwarya7 <145124330+Mekala-aishwarya7@users.noreply.github.com>
2023-09-15 17:49:22 +0530

    Update streamlit_app.py
    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ streamlit.title("my parents new healthy dinner")
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
-streamlit.text(' 🥑🍞 avacado toast')
-'
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 avacado toast')
+
